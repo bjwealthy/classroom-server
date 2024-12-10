cb7e04576cb9f2ff6e33e678c13fa561427dbaa4
Wewrangler <wewranglers@gmail.com>
2024-12-10 09:38:56 +0100

    push commit

--- Routes/TeacherRoute.js
+++ Routes/TeacherRoute.js
@@ -1,34 +1,49 @@
 import express from 'express'
-const router = express.Router();
 import con from '../utils/db.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import multer from 'multer'
-import path from 'path'
+import multer from 'multer';
+import path from 'path';
+
+const router = express.Router();
 
 router.post('/teacherlogin', (req, res) => {
-    const sql = "SELECT * FROM teachers WHERE email = ? AND password = ?"
-    con.query(sql, [req.body.email, req.body.password], (err, result) => {
+    const sql = "SELECT * FROM teachers WHERE email = ? AND password = ? AND class_id = ?"
+    con.query(sql, [req.body.email, req.body.password, req.body.class_id], (err, result) => {
         if (err) return res.json({ loginStatus: false, Error: 'Login Query Error' })
         if (result.length > 0) {
             const email = result[0].email;
+            const class_id = result[0].class_id;
+
             const token = jwt.sign(
-                { role: "teacher", email: email },
+                { role: "teacher", email: email, class_id: class_id, id: result[0].id},
                 "jwt_secret_key",
                 { expiresIn: '1d' },
             )
+
             res.cookie('token', token)
             return res.json({ loginStatus: true })
         } else {
-            return res.json({ loginStatus: false, Error: 'sT0p j0nZing niqqur!' })
+            return res.json({ loginStatus: false, Error: 'sT0p j0nZing mA nIqqUr!' })
         }
     })
 })
 
+
+//to make category list available in the frontend
 router.get('/category', (req, res) => {
     const sql = "SELECT * FROM category"
     con.query(sql, (err, result) => {
-        if (err) return res.json({ Status: false, Error: "SeelectionQuery Error" })
+        if (err) return res.json({ Status: false, Error: "Selection Query Error" })
+        return res.json({ Status: true, Result: result })
+    })
+})
+
+//to make classroom list available in the frontend
+router.get('/classroom', (req, res) => {
+    const sql = "SELECT * FROM classroom"
+    con.query(sql, (err, result) => {
+        if (err) return res.json({ Status: false, Error: "Selection Query Error" })
         return res.json({ Status: true, Result: result })
     })
 })
@@ -40,6 +55,17 @@ router.post('/add_category', (req, res) => {
         return res.json({ Status: true })
     })
 })
+
+/*
+router.post('/add_classroom', (req, res) => {
+    //const sql = "INSERT INTO classroom (`classname`) VALUES (?)"
+    const sql = `INSERT INTO classroom (classname,class_id) VALUES (?)`
+    con.query(sql, [req.body.classroom], (err, result) => {
+        if (err) return res.json({ Status: false, Error: "Bad Query Error" })
+        return res.json({ Status: true })
+    })
+})
+*/
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -56,7 +82,7 @@ const upload = multer({
 
 router.post('/add_student', upload.single('image'), (req, res) => {
     const sql = `INSERT INTO student 
-    (name,email,password,address,height,image,category_id) 
+    (name,email,password,address,debt,image,category_id,classname,class_id) 
     VALUES (?)`;
     bcrypt.hash(req.body.password, 10, (err, hash) => {
         if (err) return res.json({ Status: false, Error: "Hash Query Error" })
@@ -65,9 +91,11 @@ router.post('/add_student', upload.single('image'), (req, res) => {
             req.body.email,
             hash,
             req.body.address,
-            req.body.height,
+            req.body.debt,
             req.file.filename,
             req.body.category_id,
+            req.body.classname,
+            req.body.class_id,
         ]
         con.query(sql, [values], (err, result) => {
             if (err) return res.json({ Status: false, Error: err })
@@ -85,6 +113,15 @@ router.get('/student', (req, res) => {
     })
 })
 
+router.get('/classlist/:class_id', (req, res) => {
+    const class_id = req.params.class_id;
+    const sql = "SELECT * FROM student WHERE class_id = ?";
+    con.query(sql, [class_id], (err, result) => {
+        if(err) return res.json({Status: false})
+        return res.json(result)
+    })
+})
+
 router.get('/student/:id', (req, res) => {
     const id = req.params.id;
     const sql = "SELECT * FROM student WHERE id = ?"
@@ -97,14 +134,16 @@ router.get('/student/:id', (req, res) => {
 router.put('/edit_student/:id', (req, res) => {
     const id = req.params.id;
     const sql = `UPDATE student 
-        SET name=?, email=?, height=?, address=?, category_id=? 
+        SET name=?, email=?, address=?, debt=?, category_id=?, classname=?, class_id=? 
         WHERE id=?`
     const values = [
         req.body.name,
         req.body.email,
-        req.body.height,
         req.body.address,
-        req.body.category_id
+        req.body.debt,
+        req.body.category_id,
+        req.body.classname,
+        req.body.class_id,
     ]
     con.query(sql, [...values, id], (err, result) => {
         if (err) return res.json({ Status: false, Error: "Get Query Error" + err })
@@ -119,6 +158,44 @@ router.delete('/delete_student/:id', (req, res) => {
         if(err) return res.json({Status: false, Error: "Query Error"+err})
         return res.json({ Status: true, Result: result })
     })
+})
+
+router.get('/teacher_count', (req, res) => {
+    const sql = "SELECT count(id) AS teacher FROM teachers"
+    con.query(sql, (err, result) => {
+        if(err) return res.json({Status: false, Error: "Query Error"+err})
+        return res.json({Status: true, Result: result})
+    })
+})
+
+router.get('/student_count', (req, res) => {
+    const sql = "SELECT count(id) AS student FROM student"
+    con.query(sql, (err, result) => {
+        if(err) return res.json({Status: false, Error: "Query Error"+err})
+        return res.json({Status: true, Result: result})
+    })
+})
+
+router.get('/debt_count', (req, res) => {
+    const sql = "SELECT sum(debt) AS debtOfStudent FROM student"
+    con.query(sql, (err, result) => {
+        if(err) return res.json({Status: false, Error: "Query Error"+err})
+        return res.json({Status: true, Result: result})
+    })
+})
+
+router.get('/teacher_records', (req, res) => {
+    const sql = "SELECT * FROM teachers"
+    con.query(sql, (err, result) => {
+        if(err) return res.json({Status: false, Error: "Query Error"+err})
+        return res.json({Status: true, Result: result})
+    })
+})
+
+router.get('/logout', (req, res) => {
+    //remove token from cookie
+    res.clearCookie('token')
+    return res.json({Status: true})
 })
 
 export { router as teacherRouter }
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors'
 import { teacherRouter } from './Routes/TeacherRoute.js'; 
+import { studentRouter } from './Routes/StudentRoute.js';
+import cookieParser from 'cookie-parser';
 
 const app = express()
 app.use(cors({
@@ -9,10 +11,34 @@ app.use(cors({
     credentials: true
 }))
 app.use(express.json())
-app.use('/auth', teacherRouter)
+app.use(cookieParser())
+app.use('/teacher', teacherRouter)
+app.use('/student', studentRouter)
+
+const verifyUser = (req, res, next) => {
+    const token = req.cookies.token;
+    if(token){
+        Jwt.verify(token, "jwt_secret_key", (err, decoded) => {
+            if(err) return res.json({Status: false, Error: "Wrong token"})
+            //const email = decoded.email;
+            //const role = decoded.role;
+            req.id = decoded.id;
+            req.role = decoded.role
+            next();
+        })
+    }else{
+        return res.json({Status: false, Error: "Not authenticated!"})
+    }
+}
+
+app.get('/verify', verifyUser, (req, res) => {
+    return res.json({Status: true, role: req.role, id: req.id})
+})
+
+
 app.use(express.static('public'))
 
 //server listens on port 3000
 app.listen(3000, () => {
-    console.log('server is a running')
+    console.log('express server is a running')
 })
