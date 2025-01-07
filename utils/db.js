11be062b166eabaf76a97b50aa7192e987c290e3
Wewrangler <wewranglers@gmail.com>
2025-01-07 15:30:21 +0100

    db info added

--- utils/db.js
+++ utils/db.js
@@ -2,10 +2,16 @@
 import mysql from 'mysql';
 
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'studentms'
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'studentms'
+// })
+
+host: 'classroom-bjwealthy-classroom.g.aivencloud.com',
+user: 'avnadmin',
+password: 'AVNS_eJM3jK18_Jnu5vZzeLV',
+database: 'defaultdb'
 })
 
 con.connect(function(err){
@@ -18,24 +24,3 @@ con.connect(function(err){
 })
 
 export default con;
-
-/*
-import mysql from 'mysql';
-const connection = mysql.createConnection({
-    host: 'localhost',     // host for connection
-    port: 3306,            // default port for mysql is 3306
-    database: 'studentms',      // database from which we want to connect our node application
-    user: 'root',          // username of the mysql connection
-    password: ''       // password of the mysql connection
-});
-
-// executing connection
-connection.connect(function(err) {
-    if (err) {
-        console.log("error occurred while connecting");
-    } else {
-        console.log("connection created with mysql successfully");
-    }
-});
-export default connection;
-*/
