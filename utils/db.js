f1665e229d2c30ab9a827ad0a47696cdd3637ce3
Wewrangler <wewranglers@gmail.com>
2025-01-09 10:43:31 +0100

    to be added online

--- utils/db.js
+++ utils/db.js
@@ -2,16 +2,14 @@
 import mysql from 'mysql';
 
 const con = mysql.createConnection({
-    //     host: 'localhost',
-    //     user: 'root',
-    //     password: '',
-    //     database: 'studentms'
-    // })
-
-    host: 'classroom-bjwealthy-classroom.g.aivencloud.com',
-    user: 'avnadmin',
-    password: 'AVNS_eJM3jK18_Jnu5vZzeLV',
-    database: 'defaultdb'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'studentms'
+    // host: 'classroom-bjwealthy-classroom.g.aivencloud.com',
+    // user: 'avnadmin',
+    // password: 'AVNS_eJM3jK18_Jnu5vZzeLV',
+    // database: 'defaultdb'
 })
 
 con.connect(function(err){
