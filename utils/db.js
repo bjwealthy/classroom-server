47ef9a156c19366ce2a723033521522912f4caad
Mobolaji Adesoye <bjwealthy@gmail.com>
2025-01-11 20:03:48 +0100

    Update db.js again

--- utils/db.js
+++ utils/db.js
@@ -2,10 +2,11 @@
 import mysql from 'mysql';
 
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'studentms'
+    host: 'classroom-bjwealthy-classroom.g.aivencloud.com',
+    port: '15044',
+    user: 'avnadmin',
+    password: 'AVNS_eJM3jK18_Jnu5vZzeLV',
+    database: 'defaultdb'
 })
 
 con.connect(function(err){
