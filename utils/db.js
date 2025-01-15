f903dce0810822ad1ffeccd0a119590f24e1c68e
Wewrangler <wewranglers@gmail.com>
2025-01-15 13:45:55 +0100

    some message

--- utils/db.js
+++ utils/db.js
@@ -1,12 +1,19 @@
 import mysql from 'mysql';
 
+// const con = mysql.createConnection({
+//     host: 'classroom-bjwealthy-classroom.g.aivencloud.com',
+//     port: '15044',
+//     user: 'avnadmin',
+//     password: 'AVNS_eJM3jK18_Jnu5vZzeLV',
+//     database: 'defaultdb'
 const con = mysql.createConnection({
-    host: 'classroom-bjwealthy-classroom.g.aivencloud.com',
-    port: '15044',
-    user: 'avnadmin',
-    password: 'AVNS_eJM3jK18_Jnu5vZzeLV',
-    database: 'defaultdb'
+    host: 'sql.freedb.tech',
+    port: '3306',
+    user: 'freedb_bjwealthy',
+    password: '9Kyc@*#X9psX*bY',
+    database: 'freedb_bjwealthy-classroom-db'
 })
+
 
 con.connect(function(err){
     if(err){
