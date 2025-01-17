df2d754651c36132136373d41a02b9977db96a1e
Wewrangler <wewranglers@gmail.com>
2025-01-17 14:25:53 +0100

    student route edited

--- Routes/StudentRoute.js
+++ Routes/StudentRoute.js
@@ -8,7 +8,7 @@ import path from 'path'
 const router = express.Router()
 
 router.post('/studentlogin', (req, res) => {
-    alert('hi');
+    res.send('hello everyone');
     const sql = "SELECT * FROM student WHERE email = ? AND class_id = ?"
     con.query(sql, [req.body.email, req.body.class_id], (err, result) => {
         if (err) return res.json({ loginStatus: false, Error: "Login Query Error" })
