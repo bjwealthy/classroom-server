dc3925b61a4625a4e623178a11481dc6c14149ce
Wewrangler <wewranglers@gmail.com>
2025-01-17 13:05:52 +0100

    pgsql db credentials added

--- utils/db.js
+++ utils/db.js
@@ -6,13 +6,20 @@ import mysql from 'mysql';
 //     user: 'avnadmin',
 //     password: 'AVNS_eJM3jK18_Jnu5vZzeLV',
 //     database: 'defaultdb'
-const con = mysql.createConnection({
-    host: 'sql.freedb.tech',
-    port: '3306',
-    user: 'freedb_bjwealthy',
-    password: '9Kyc@*#X9psX*bY',
-    database: 'freedb_bjwealthy-classroom-db'
-})
+
+//     host: 'sql.freedb.tech',
+//     port: '3306',
+//     user: 'freedb_bjwealthy',
+//     password: '9Kyc@*#X9psX*bY',
+//     database: 'freedb_bjwealthy-classroom-db'
+
+    var con = mysql.createConnection({
+        host: 'dpg-cu547oggph6c73dtls7g-a',
+        port: '543',
+        database: 'classroompgsql',
+        user: 'classroompgsql_user',
+        password: '0o4Rm4IxXTQzdswcPNe9zvY1SyQBllrz'
+});
 
 
 con.connect(function(err){
